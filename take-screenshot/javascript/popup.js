02656934bf7e45f6b2dbbd401f4e235afda1ac90
Matt Kirtley <54330969+MKirtley@users.noreply.github.com>
2023-07-10 15:33:00 +0100

    Delete take-screenshot directory

--- take-screenshot/javascript/popup.js
+++
@@ -1,6 +0,0 @@
-/**
- * Send the "takeScreenshot" message when the button on popup.html is pressed.
- */
-document.getElementById("takeScreenshot").addEventListener("click", function () {
-    chrome.runtime.sendMessage({ msg: "takeScreenshot" });
-});
